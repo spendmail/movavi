9eed2a216f536d417ba46f6866cba4e068d34329
Sizikov Konstantin <spendlively@mail.ru>
2018-08-20 12:29:17 +0700

    Test

---
+++ src/Application.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: spendlively
+ * Date: 20.08.18
+ * Time: 12:24
+ */
+
+namespace Movavi;
+
+
+class Application
+{
+
+    public function run()
+    {
+        var_dump('Hello, World!');
+    }
+}
